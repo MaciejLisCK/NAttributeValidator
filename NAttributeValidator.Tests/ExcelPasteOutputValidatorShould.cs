846944d8f5cd45851ed25333b17b299def133798
MaciejLisCK <accounts@maciejlis.com>
2013-09-04 17:10:55 +0100

    Tests

---
+++ NAttributeValidator.Tests/ExcelPasteOutputValidatorShould.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NAttributeValidator.Helpers;
+using NAttributeValidator.Validation;
+
+namespace NAttributeValidator.Tests
+{
+    [TestClass]
+    public class ExcelPasteOutputValidatorShould
+    {
+        readonly ExcelPasteOutputValidator _validator = new ExcelPasteOutputValidator(new StringToMatrixParser("\n", "\t"), 3);
+
+        [TestMethod]
+        public void ReturnNoErrorForCorrectColumnsAmount()
+        {
+            var input = "Test1	Test2	Test3";
+
+            var result = _validator.Validate(input);
+
+            Assert.IsTrue(result.IsValid);
+        }
+
+        [TestMethod]
+        public void ReturnOneErrorForToFewColumnAmount()
+        {
+            var input = "Test1	Test2";
+
+            var result = _validator.Validate(input);
+
+            Assert.AreEqual(result.Messages.Count(), 1);
+        }
+
+        [TestMethod]
+        public void ReturnOneErrorForToMuchColumnAmount()
+        {
+            var input = "Test1	Test2	Test3	Test4";
+
+            var result = _validator.Validate(input);
+
+            Assert.AreEqual(result.Messages.Count(), 1);
+        }
+
+    }
+}
