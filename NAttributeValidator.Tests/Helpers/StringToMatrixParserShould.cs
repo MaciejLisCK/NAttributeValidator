ebdaaf0a25e626a33e5820d5ca38b5f7a7b6044c
MaciejLisCK <accounts@maciejlis.com>
2013-09-05 00:36:48 +0100

    StrinToMatrixTests

---
+++ NAttributeValidator.Tests/Helpers/StringToMatrixParserShould.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NAttributeValidator.Helpers;
+
+namespace NAttributeValidator.Tests.Helpers
+{
+    [TestClass]
+    public class StringToMatrixParserShould
+    {
+        readonly StringToMatrixParser _mother = new StringToMatrixParser("\n","\t");
+
+        [TestMethod]
+        public void ReturnValidColumnsCountForSingleRow()
+        {
+            var input = "Test1\tTest2\tTest3";
+
+            var result = _mother.Parse(input);
+
+            Assert.AreEqual(result[0].Count, 3);
+        }
+
+        [TestMethod]
+        public void ReturnValidColumnsCountForMultipleRow()
+        {
+            var input = "Test1\tTest2\tTest3\nTest1\tTest2";
+
+            var result = _mother.Parse(input);
+
+            Assert.AreEqual(result[0].Count, 3);
+            Assert.AreEqual(result[1].Count, 2);
+        }
+
+        [TestMethod]
+        public void ReturnValidRowsCountForSingleRow()
+        {
+            var input = "Test1\tTest2\tTest3";
+
+            var result = _mother.Parse(input);
+
+            Assert.AreEqual(result.Count, 1);
+        }
+
+        [TestMethod]
+        public void ReturnValidRowsCountForMultipleRow()
+        {
+            var input = "Test1\tTest2\tTest3\nTest1\tTest2";
+
+            var result = _mother.Parse(input);
+
+            Assert.AreEqual(result.Count, 2);
+        }
+    }
+}
